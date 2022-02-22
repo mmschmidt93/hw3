7170d9a35c4fdc154fd5648e3f16729977df12e1
mmschmidt93 <matt.schmidt@kellogg.northwestern.edu>
2022-02-22 02:49:58 +0000

    adding show to website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  resources :places
+
+  resources :posts
+
+  # Defines the root pa th route ("/")
   # root "articles#index"
 end
