e8ede4acfe252db50663f3343553c6192381d207
mmschmidt93 <matt.schmidt@kellogg.northwestern.edu>
2022-02-22 05:35:25 +0000

    watching lecture

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,28 @@
 class PlacesController < ApplicationController
+
+    #params["KEY"] access to whatever is in params code
+    # @KEY = params["KEY"]
+    #Can use this @KEY in code
+
+    # in index <form action="/places" method="GET"> <input name = "name" type = text"> 
+    #<button>Let's play</button></form>
+
+    def index
+        #@companies = Company.all
+    end
+
+    def show
+        #@company = Company.find(params["id"])
+    end
+
+    def new
+        #@company = Company.new
+    end
+
+    def create
+        #@company = Company.new(params["company"])
+        #@company.save
+        #redirect_to "/companies"
+    end
+
 end
