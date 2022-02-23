65af5bb83ef784b5d6d3438726398fe0ca7b6b14
mmschmidt93 <matt.schmidt@kellogg.northwestern.edu>
2022-02-23 00:52:08 +0000

    successfully conquering dates

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,5 @@
 class PlacesController < ApplicationController
 
-    #params["KEY"] access to whatever is in params code
-    # @KEY = params["KEY"]
-    #Can use this @KEY in code
-
     # in index <form action="/places" method="GET"> <input name = "name" type = text"> 
     #<button>Let's play</button></form>
 
